4693c69e109526dac777c4c9863112b408873dea
DMDXV <Padfootdiamond@Gmail.com>
2020-11-22 18:14:55 +0000

    Index Page added

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex h-screen bg-gray-50 dark:bg-gray-900" id="app">
+    <Sidebar />
+
+    <div class="flex flex-col flex-1 w-full">
+      <Navigation />
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue'
+import Navigation from './components/Navigation.vue'
+export default {
+  name: 'App',
+  components: {
+    Sidebar,
+    Navigation
+  }
+}
+</script>
